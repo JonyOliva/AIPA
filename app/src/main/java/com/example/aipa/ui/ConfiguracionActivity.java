a0baaf7ee4e165fba62a63d99239400c1851589c
JuanAgosta20 <juan.agosta@hotmail.com>
2020-10-24 11:10:33 -0300

    Pantallas RegistrarUsuarioActivity.java, ConfiguracionActivity.java y FichaDiariaActivity.java

--- app/src/main/java/com/example/aipa/ui/ConfiguracionActivity.java
+++ app/src/main/java/com/example/aipa/ui/ConfiguracionActivity.java
@@ -1,14 +1,16 @@
-package com.example.aipa;
+package com.example.aipa.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.aipa.R;
+
+public class ConfiguracionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_configuracion);
     }
 }

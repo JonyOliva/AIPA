79ad10d5ad6e191f38a2e8fc5f37169410d0ec38
Julireda <50599949+Julireda@users.noreply.github.com>
2020-11-13 20:29:39 -0300

    lista en ficha anterior(sin probar)

--- app/src/main/java/com/example/aipa/ui/FichaAnteriorActivity.java
+++ app/src/main/java/com/example/aipa/ui/FichaAnteriorActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.aipa.R;
@@ -13,8 +15,10 @@ import com.example.aipa.R;
 import java.util.ArrayList;
 
 import Gestion.FichasGestion;
+import Gestion.IngredientesGestion;
 import Gestion.SintomasGestion;
 import Models.FichaDiaria;
+import Models.Ingrediente;
 import Models.Sintoma;
 
 public class FichaAnteriorActivity extends AppCompatActivity {
@@ -22,6 +26,7 @@ public class FichaAnteriorActivity extends AppCompatActivity {
     TextView Comentario;
     TextView Ejercicio;
     TextView Sintomas;
+    ListView lstIngredientes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +36,7 @@ public class FichaAnteriorActivity extends AppCompatActivity {
         Comentario= (TextView)findViewById(R.id.txtFAComentario);
         Fecha = (TextView)findViewById(R.id.txtFAfecha);
         Sintomas = (TextView)findViewById(R.id.txtFASintomas);
-
+lstIngredientes=(ListView)findViewById(R.id.lstFAingredientes);
 
         String fecha = getIntent().getStringExtra("fecha");
         if (fecha != null){
@@ -55,7 +60,17 @@ public class FichaAnteriorActivity extends AppCompatActivity {
         ArrayList<Sintoma> listasintomas = sg.getAll();
 
         Sintomas.setText(listasintomas.get(ficha.getSintoma().getIdSintoma()).getDescripcion());
+        fillIngredients(ficha.getIdFicha());
+    }
 
+    public void fillIngredients(int idficha){
+        IngredientesGestion ig = new IngredientesGestion();
+        ArrayList <Ingrediente> listaingredientes = ig.getIngredientesXFicha(idficha);
+
+        if (listaingredientes!=null){
+
+            ArrayAdapter adapter = new ArrayAdapter<Ingrediente>(this,R.layout.lista_ingredientes,listaingredientes);
+            lstIngredientes.setAdapter(adapter); }
     }
 
 
